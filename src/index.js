24c87975058dbaf0aa694a26d123aa17fa6ff709
CAEB <1912401521@qq.com>
2017-06-14 20:52:22 +0800

    Create index.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import React from 'react'
 import ReactDom from 'react-dom';
 import App from './components/App/App';
-/*`https://api.github.com/search/users?q=${searchName}`*/
 
 ReactDom.render(<App/>,document.getElementById('root'));
